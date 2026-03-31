6eb6f1fecaf470d805c4064c48270131f86a3fac
David Smith <david@bcits.com>
2026-03-31 15:22:35 +0530

    David register customer router and add health api for Customer Portal

--- app/main.py
+++ app/main.py
@@ -1,7 +1,14 @@
 from fastapi import FastAPI
+from app.routes import customer
 
 app = FastAPI()
+
+app.include_router(customer.router)
 
 @app.get("/")
 def home():
     return {"message": "Customer Portal API Running"}
+
+@app.get("/health")
+def health():
+    return {"status":"ok"}
